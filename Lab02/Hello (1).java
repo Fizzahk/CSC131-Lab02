e2419b5481891043e2507d42e7dc5423a1c0ba58
g_gio <g_gio@10.117.146.163>
2018-11-01 16:46:20 -0700

    Late for the party

--- Lab02/Hello (1).java
+++ Lab02/Hello (1).java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("218953385 - This line was added by Fizzah Khan.");
 		System.out.println("218674717 - This line was added by Pawan Khatri (PK).");
 		System.out.println("219514621 - This line was added by Oleg Seliatenko.");
-		System.out.println("217887749 - This line was added by Giovanni Gonzalez.");
+		System.out.println("217887749 - This line was added by Giovanni Gonzalez");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
